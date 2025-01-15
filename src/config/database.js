5dc75b288e3f34234493bd265295a9a70d56caa6
DevExpert250802 <devkhandelwal25082002@gmail.com>
2025-01-16 04:16:37 +0530

    updated db password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@
 
  const connectDB = async () =>{
     await mongoose.connect(
-        "mongodb+srv://NamasteNode:admin@namastenode.0fpmy.mongodb.net/MergeMates"
+        "mongodb+srv://NamasteNode:302020DkPs@namastenode.0fpmy.mongodb.net/MergeMates"
     );
  };
  module.exports = connectDB;
