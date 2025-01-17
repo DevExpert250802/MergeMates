38676882eb0239143f6029b06932f2d5a7d48e18
DevExpert250802 <devkhandelwal25082002@gmail.com>
2025-01-17 11:08:35 +0530

    port Change

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ connectDB()
     .then(() => {
         console.log('DB connected');
         app.listen(process.env.PORT, () => {
-            console.log('Server is successfully listening on port 3000');
+            console.log('Server is successfully listening on port 4000');
         });
     })
     .catch((err) => {
